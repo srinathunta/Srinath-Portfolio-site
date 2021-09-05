865095dc7a6fde252f5a2d59e17597c37e287181
demoweb1 <demoweb1@demoweblink.com>
2021-09-05 11:18:15 +0530

    first commit

---
+++ src/components/main-banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Profile from '../images/profile-demo.jpg'
+export default function Banner(){
+    return(
+        <div className='banner'>
+            <div className="text">
+                <div className='slide-left banner-text'>
+                    <img className="profile-image" src={Profile} alt="profile-image"/>
+                </div>
+                <div className="vl"></div>
+                <div className='slide-right banner-text'><h2>Srinath<br />Unta</h2></div>
+            </div>
+            
+        </div>
+    )
+}
