7af99fa0560a205f623020cee0688028f37c1a27
demoweb1 <demoweb1@demoweblink.com>
2021-09-19 11:10:32 +0530

    first commit

--- src/components/main-banner.js
+++ src/components/main-banner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactTypingEffect from 'react-typing-effect';
 import 'boxicons';
-import BackImg from '../images/profile/new.jpg'
+import BackImg from '../images/profile/new-1.jpg'
 
 export default function Banner(){
   const htmltext = ''
@@ -14,10 +14,10 @@ export default function Banner(){
                 <p><ReactTypingEffect  text={["Full-stack Developer."]} /></p>
                 
                 <div className="social-links">
-                    <a href="#" className="facebook"><i className="bx bxl-facebook"></i></a>
-                    <a href="#" className="instagram"><i className="bx bxl-instagram"></i></a>
-                    <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a>
-                    <a href="#" className="linkedin"><i className="bx bxl-linkedin"></i></a>
+                    <a href="https://www.facebook.com/srinath.unta" className="facebook"><i className="bx bxl-facebook"></i></a>
+                    <a href="https://www.instagram.com/srinath_unta/" className="instagram"><i className="bx bxl-instagram"></i></a>
+                    {/* <a href="#" className="google-plus"><i className="bx bxl-skype"></i></a> */}
+                    <a href="https://www.linkedin.com/in/srinath-unta-167a95116/" className="linkedin"><i className="bx bxl-linkedin"></i></a>
             </div>
         </div>
   </section>
